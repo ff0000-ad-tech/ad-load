b6c9c038ba356ab6b442c8b997d5988cc8c4f219
Derek Miranda <derek.miranda.1992@gmail.com>
2019-12-02 16:40:53 -0800

    Uses FBA data to blobs fn from ad-global

--- index.js
+++ index.js
@@ -104,6 +104,8 @@
 	- ? comment out progress calculations
 */
 
+import { fbaDataToBlobs } from 'ad-global'
+
 import { LoaderBase } from './lib/mixin/LoaderBase.js'
 import * as Utils from './lib/mixin/LoaderUtils.js'
 import ImageLoader from './lib/single/ImageLoader.js'
@@ -333,14 +335,12 @@ export default class Loader extends LoaderBase {
 
 	// -------------------------------------------------------------------------------------------------------------
 	// PRIVATE METHODS
-	_addSingleLoad(url, fbaOverwrite) {
-		// console.log('_addSingleLoad()', url, fbaOverwrite)
+	_addSingleLoad(url, fileName, fileTypeOverwrite) {
 		const L = this
 
-		// fbaOverwrite is an array [ file name, file extension ]
-		const fileType = fbaOverwrite ? fbaOverwrite[1] : L.fileType || Utils.getFileType(url)
+		// fileTypeOverwrite can be a string of the explicit file type
+		const fileType = fileTypeOverwrite || L.fileType || Utils.getFileType(url)
 		let loaderType
-		// console.log('\t fileType:', fileType)
 
 		switch (fileType) {
 			case 'jpg':
@@ -366,7 +366,7 @@ export default class Loader extends LoaderBase {
 		}
 
 		// either the data as binary OR the file path and name
-		const urlChoice = fbaOverwrite ? url : L.prepend + url
+		const urlChoice = fileTypeOverwrite ? url : L.prepend + url
 		// console.log('\t url:', url, '| loaderType:', loaderType)
 
 		var singleLoader = new loaderType(urlChoice, {
@@ -382,8 +382,8 @@ export default class Loader extends LoaderBase {
 
 		// from fba, the files are binary, so there is
 		// no file name to reference so set it here
-		if (fbaOverwrite) {
-			singleLoader.fileName = fbaOverwrite[0]
+		if (fileName) {
+			singleLoader.fileName = fileName
 		}
 
 		singleLoader.queueIndex = L._total
@@ -406,75 +406,10 @@ export default class Loader extends LoaderBase {
 	}
 
 	_addFbaSubLoads(loader) {
-		// console.log("_addFbaSubLoads()", loader)
-
-		// Conversion between uint8s and uint32s.
-		let uint8 = new Uint8Array(4)
-		let uint32 = new Uint32Array(uint8.buffer)
-
-		// start after = signature(8 bytes) + IHDR(25 bytes) + fbAc(16 bytes total, but only 11: size,type,content-1 )
-		let idx = 44
-
-		const chunkTotal = new Uint8Array(loader.dataRaw, idx, 1)[0]
-		//console.log( 'number of images as chunks:', chunkTotal )
-
-		// skip over rest of fbAc chunk: count value byte + CRC 4 bytes
-		idx += 5
-
-		for (let i = 0; i < chunkTotal; i++) {
-			// size, type, content, crc
-			// get the size of next chunk as on UintArray
-			let sizeOfChunk = new Uint8Array(loader.dataRaw, idx, 4)
-
-			// Read the length of the current chunk, which is stored as a Uint32.
-			// this one has to be a loop, as values get assigned to uint8, that changes buffer of uint32
-			// also, the values must be set reversed, henced the count down loop
-			let up = 4
-			for (var k = 0; k < 4; k++) {
-				//console.log( k, up, sizeOfChunk[k] )
-				uint8[--up] = sizeOfChunk[k]
-			}
-
-			// all chunk data NOT including the type
-			let length = uint32[0]
-
-			idx += 7
-
-			// Get the chunk type in ASCII, only last character really matters
-			let type = String.fromCharCode.apply(String, new Uint8Array(loader.dataRaw, idx++, 1))
-
-			// console.log('\ttype:', type, '\tlength:', length)
-			let fileNameLengthArray = new Uint8Array(loader.dataRaw, idx + 3, 1)
-			let fileNameLength = fileNameLengthArray[0] || 0 // default to zero incase array fails? maybe unnecessary
-
-			let nameBuffer = new Uint8Array(loader.dataRaw, idx + 4, fileNameLength)
-			let fileName = String.fromCharCode.apply(String, nameBuffer)
-
-			// first add to the file name length 4 bytes: file name length byte count
-			fileNameLength += 4
-
-			// offset the array start and length by the file name length
-			let chunkData = new Uint8Array(loader.dataRaw, idx + fileNameLength, length - fileNameLength)
-			// NOTE: ArrayBuffer.slice() does not exist in IE10, so have to do it manually inline
-			//var chunkData = new Uint8Array(chunk.buffer.slice(4))
-
-			// skip over the content AND skip over the CRC value by incrementing by 4 bytes
-			idx += length + 4
-
-			let fileNameSplit = fileName.split('.')
-			//var blobFileType = '';// 'application/octet-stream';
-			let blobFileType
-			if (type === 'f') {
-				blobFileType = 'application/' + (fileNameSplit[1] === 'ttf' ? 'x-font-ttf' : `font-${fileNameSplit[1]}`)
-			} else {
-				blobFileType = 'image/' + (fileNameSplit[1] == 'svg' ? 'svg+xml' : fileNameSplit[1])
-			}
-			let blob = new Blob([chunkData], { type: blobFileType })
-			let url = URL.createObjectURL(blob)
-			// url will be ~ blob:32c3c7af-ef04-414f-a073-685602fe8a28
-			// console.log(fileNameSplit, blobFileType, url)
-			this._addSingleLoad(url, fileNameSplit)
-		}
+		const blobs = fbaDataToBlobs(loader.dataRaw)
+		blobs.forEach(blob => {
+			this._addSingleLoad(blob.src, blob.path, blob.fileType)
+		})
 	}
 
 	_startSingleLoad(i) {
